fd673fc9bf5791a03aaab258f34d75a4974a9532
renga.r.santh.ledge <renga.r.santh.ledge@CDC2-D-44P4N62.dir.svc.accenture.com>
2017-06-01 19:56:15 +0530

    Added Login user pojo

---
+++ src/main/java/com/acc/dto/LoginUser.java
@@ -0,0 +1,33 @@
+package com.acc.dto;
+
+public class LoginUser {
+	Long employeeId;
+	String employeeName;
+	String password;
+	Boolean enabled;
+	public Long getEmployeeId() {
+		return employeeId;
+	}
+	public void setEmployeeId(Long employeeId) {
+		this.employeeId = employeeId;
+	}
+	public String getEmployeeName() {
+		return employeeName;
+	}
+	public void setEmployeeName(String employeeName) {
+		this.employeeName = employeeName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Boolean getEnabled() {
+		return enabled;
+	}
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+}
